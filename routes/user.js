1f68895e76536828444307e9855772e3331f05ff
devparkoon <jh.park@qualson.com>
2020-02-05 17:55:00 +0900

    [기능] 회원가입 API 추가

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const router = express.Router()
 
+// Model
+const User = require('../model/user')
 
-router.post('/regist', (req, res) => {
 
+router.post('/regist', async (req, res) => {
+
+    const user = new User(req.body)
+
+    try {
+
+        const { _id, role, email, name } = await user.save()
+        res.status(200).json({
+            success: true,
+            user: {
+                _id, role, email, name
+            }
+        })
+
+    } catch (err) {
+
+        res.json({
+            success: false,
+            err
+        })
+    }
 })
 
 router.post('/login', (req, res) => {
