3293fbcdeb3b70df13ebe527bb1d97e500dbc184
devparkoon <jh.park@qualson.com>
2020-02-06 11:21:10 +0900

    [기능] 로그인 API 추가

--- routes/user.js
+++ routes/user.js
@@ -28,8 +28,21 @@ router.post('/regist', async (req, res) => {
     }
 })
 
-router.post('/login', (req, res) => {
-    console.log('user login request')
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const foundUser = await User.findOne({ email })
+        if (!foundUser) return res.status(401).json({ success: false, message: '이메일을 확인해주세요.' })
+
+        if (foundUser.password !== password) return res.status(401).json({ success: false, message: '비밀번호를 확인해주세요.' })
+
+        return res.json({
+            success: true,
+        })
+    } catch (err) {
+        res.status(500).json({ err })
+    }
 })
 
 module.exports = router
