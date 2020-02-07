8701f3c0a0f88b7d10a6273284a75a439e923a1e
devparkoon <jh.park@qualson.com>
2020-02-07 11:57:42 +0900

    [기능] 상품 생성 및 조회 (인증X)
     * 상품 조회는 누구나 할 수 있지만, 상품을 생성하는 것에는 제한을   둬야한다.
     * 토큰 체크 필요
     * populate: 데이터를 덧 붙이다. Object Id 를 참고하고 있었다면, 그 id 정보를 덧 붙일 수 있다.

---
+++ routes/product.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const Product = require('../model/product')
+const router = express.Router()
+
+// get all products
+router.get('/', async (req, res) => {
+    const foundProducts = await Product.find().populate({
+        path: 'author',
+        select: '_id name'
+    })
+
+    res.json({
+        success: true,
+        products: foundProducts
+    })
+})
+
+// set product
+router.post('/', async (req, res) => {
+
+    const product = new Product(req.body)
+
+    try {
+        const savedProduct = await product.save()
+        res.json({ success: true, product: savedProduct })
+    } catch (err) {
+        res.json({ success: false, err })
+    }
+})
+
+module.exports = router
